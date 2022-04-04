d6f6f4bb0faaf781b7d7aa4ba65b49184ab4e9ca
Роман Княжский <knyazhskiy@inbox.ru>
2022-04-04 16:02:58 +0300

    7

---
+++ DinoBoard/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Knyaze\source\situational-center\DinoBoard\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
